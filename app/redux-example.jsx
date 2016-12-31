6ce7245707043d95e8ad17790eec9361c7c2e10a
Sergey <sergavo@gmail.com>
2016-12-31 15:39:07 +0300

    Add subscribe and dev tools

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -15,7 +15,18 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
       return state;
   }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+var unsubscribe = store.subscribe(() => {
+  var state = store.getState();
+
+  console.log('Name is', state.name);
+  document.getElementById('app').innerHTML = state.name;
+});
+//unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState', currentState);
@@ -24,4 +35,10 @@ store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Andrew'
 });
-console.log('Name should be Andrew', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Emily'
+});
+
+//console.log('Name should be Andrew', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -18,12 +18,31 @@ var reducer = (state = stateDefaul, action) => {
       return state;
   }
 };
-var store = redux.createStore(reducer);
+var store = redux.createStore(reducer, redux.compose(
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
+// Subscribe to changes
+store.subscribe(() => {
+  var state = store.getState();
+
+  document.getElementById('app').innerHTML = state.searchText;
+});
 
 console.log('currentState', store.getState());
 
 store.dispatch({
   type: 'CHANGE_SEARCH_TEXT',
   searchText: 'work'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'dog'
+});
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'something else'
 });
 console.log('searchText should be "work"', store.getState());
